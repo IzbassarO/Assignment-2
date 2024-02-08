1d14b5c9d5360dadf0f0ce41b570634baa4c997a
Izbassar <izok2004@gmail.com>
2024-02-08 20:54:43 +0600

    Add Procfile and modify bcryptjs dependency

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const path = require('path');
 const axios = require('axios');
 const { MongoClient } = require('mongodb');
